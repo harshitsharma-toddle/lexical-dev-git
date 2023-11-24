4efe3a68747667a714501ab06a41461ec98d4669
harshitsharma-toddle <harshit.sharma@toddleapp.com>
2023-11-24 13:25:29 +0530

    Basic config: lexical editor

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Editor from "./Editor";
 
 function App() {
   return (
@@ -6,6 +7,7 @@ function App() {
       <h1 style={{ textAlign: "center", margin: "8px" }}>
         Lexical Dev [Study]
       </h1>
+      <Editor />
     </div>
   );
 }
---
+++ src/Editor.js
@@ -0,0 +1,23 @@
+import { LexicalComposer } from "@lexical/react/LexicalComposer";
+import { PlainTextPlugin } from "@lexical/react/LexicalPlainTextPlugin";
+import { ContentEditable } from "@lexical/react/LexicalContentEditable";
+import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
+
+const theme = {};
+
+export default function Editor() {
+  const initialConfig = {
+    namespace: "lexical-dev-editor",
+    theme,
+  };
+
+  return (
+    <LexicalComposer initialConfig={initialConfig}>
+      <PlainTextPlugin
+        contentEditable={<ContentEditable />}
+        placeholder={<div>Enter some text...</div>}
+        ErrorBoundary={LexicalErrorBoundary}
+      />
+    </LexicalComposer>
+  );
+}
