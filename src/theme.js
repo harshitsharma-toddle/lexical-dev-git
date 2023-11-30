297f2af9a3d53ce70a7ad9f2652248e6adf2f491
harshitsharma-toddle <harshit.sharma@toddleapp.com>
2023-12-01 03:33:23 +0530

    Add: heading node classes

--- src/theme.js
+++ src/theme.js
@@ -1,5 +1,13 @@
 import "./debugMode.scss";
 
-const theme = {};
+const theme = {
+  text: {
+    bold: "lexical-dev-text-bold",
+  },
+  heading: {
+    h1: "lexical-dev-heading-h1",
+    h2: "lexical-dev-heading-h2",
+  },
+};
 
 export default theme;
